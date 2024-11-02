a9ec2860218cfc9836aa951b0bf32757dbeac285
Felipelesmes <64442854+Felipelesmes@users.noreply.github.com>
2024-11-01 20:41:55 -0500

    Update Cocina.java

--- Proyectos/Cocina.java
+++ Proyectos/Cocina.java
@@ -1,20 +1,31 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
-    	public int expectedMinutesInOven(){
-        return 40;
-    }
-    // TODO: define the 'remainingMinutesInOven()' method
-    public int remainingMinutesInOven(int minInOven){
-        return expectedMinutesInOven() - minInOven;
+    
+    // Método principal
+    public static void main(String[] args) {
+        Lasagna lasagna = new Lasagna();
+        System.out.println("Expected minutes in oven: " + lasagna.expectedMinutesInOven());
+        System.out.println("Remaining minutes in oven: " + lasagna.remainingMinutesInOven(20));
+        System.out.println("Preparation time in minutes: " + lasagna.preparationTimeInMinutes(3));
+        System.out.println("Total time in minutes: " + lasagna.totalTimeInMinutes(3, 20));
     }
     
-    // TODO: define the 'preparationTimeInMinutes()' method
-    public int preparationTimeInMinutes(int lLayers){
+    // Método que devuelve los minutos esperados en el horno
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    // Método que devuelve los minutos restantes en el horno
+    public int remainingMinutesInOven(int minInOven) {
+        return expectedMinutesInOven() - minInOven;
+    }
+
+    // Método que calcula el tiempo de preparación en minutos
+    public int preparationTimeInMinutes(int lLayers) {
         return lLayers * 2;
     }
-    // TODO: define the 'totalTimeInMinutes()' method
-    public int totalTimeInMinutes(int lLayer, int min){
+
+    // Método que calcula el tiempo total en minutos
+    public int totalTimeInMinutes(int lLayer, int min) {
         return preparationTimeInMinutes(lLayer) + min;
     }
 }
-
